80ec1f1ff9ca599070a2d7cc3e2aaca0858c2827
Andres <afsoto.0830@gmail.com>
2018-03-05 10:19:36 -0500

    Agregue otro css

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<style type="text/css">
 		
